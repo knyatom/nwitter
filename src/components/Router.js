528ac6648cdb3a2b2a272060b6e2f5a2ea0b4374
kny <knyatom@gmail.com>
2021-08-21 00:30:15 +0900

    1230

---
+++ src/components/Router.js
@@ -0,0 +1,11 @@
+import {HashRounter as Router, Route,Switch}  from 'react-router-dom';
+
+const AppRouter=()=>{
+    return(
+    <Router>
+        <Switch>
+
+        </Switch>
+    </Router>
+    )
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 import firebase from "firebase/app";
 
 const  firebaseConfig = {
-    apiKey: "AIzaSyA3qNd2eAgAxLjcb0YMkFrEv039hVNyy7g",
-    authDomain: "nwitter-878a5.firebaseapp.com",
-    projectId: "nwitter-878a5",
-    storageBucket: "nwitter-878a5.appspot.com",
-    messagingSenderId: "364953097562",
-    appId: "1:364953097562:web:ffc6d7d3b6cd0b10740f63"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID,
   };
+
+  export default firebase.initializeApp(firebaseConfig);
