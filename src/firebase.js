a27876e624a7247f12feb5916016e28fac6dd50d
kny <knyatom@gmail.com>
2021-08-20 22:59:36 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+
+const  firebaseConfig = {
+    apiKey: "AIzaSyA3qNd2eAgAxLjcb0YMkFrEv039hVNyy7g",
+    authDomain: "nwitter-878a5.firebaseapp.com",
+    projectId: "nwitter-878a5",
+    storageBucket: "nwitter-878a5.appspot.com",
+    messagingSenderId: "364953097562",
+    appId: "1:364953097562:web:ffc6d7d3b6cd0b10740f63"
+  };
